f15ef4857888eed7d94907bd5a96c6dbd2c3f449
trinetry <stro2007@ukr.net>
2022-01-08 19:33:15 +0200

    Create Statistics.styled.js

---
+++ src/components/Statistics.styled.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const SectionStyled = styled.section`
+  display: block;
+  margin: 0px auto;
+  margin-top: 30px;
+  width: 300px;
+  padding: 20px;
+  border: solid 3px silver;
+  border-radius: 20px;
+  background-color: #234;
+  color: white;
+  text-align: center;
+  font-family: verdana;
+`;
+
+export const H2Styled = styled.h2`
+  text-align: center;
+  color: white;
+  font-size: 8pt;
+  margin-top: 0px;
+  margin-bottom: 10px;
+`;
+
+export const UlStyled = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+`;
+
+export const LiStyled = styled.li`
+  display: block;
+  width: 50%;
+  border: solid 0px black;
+  border: 1px 0px 0px 0;
+  padding: px;
+  margin: 0px;
+  font-size: 9pt;
+  font-weight: 100;
+`;
+
+export const LiHeaderStyled = styled.span`
+  display: block;
+  padding: 5px 0;
+  color: white;
+`;
+
+export const LiNumbersStyled = styled.span`
+  display: block;
+  padding: 5px 0;
+  color: #f54661;
+  font-size: 9pt;
+  font-weight: 600;
+`;
